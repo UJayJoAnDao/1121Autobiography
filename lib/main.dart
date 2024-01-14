5e37ec4e96752f5d73054f02d4084cfdef5ec5ad
UJay <plmoknijb1234510@gmail.com>
2024-01-15 00:23:02 +0800

    all commit

---
+++ lib/main.dart
@@ -0,0 +1,205 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+final player=AudioPlayer()..setReleaseMode(ReleaseMode.loop);
+
+void main()=>runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  final tabs=[
+    screen1(),
+    screen2(),
+    screen3(),
+    screen4(),
+  ];
+
+  int previousIndex=0;
+  int currentIndex=0;
+
+  @override
+  Widget build(BuildContext context) {
+    if (currentIndex==0) player.play(AssetSource("1.mp3"));
+    return MaterialApp(
+      title: '我的自傳',
+      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+                       useMaterial3: true,),
+      home: Scaffold(
+        appBar: AppBar(title: Text('我的自傳'),),
+        body: tabs[currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.black,
+          selectedItemColor: Colors.white,
+          selectedFontSize: 18,
+          unselectedFontSize: 14,
+          iconSize: 30,
+          currentIndex: currentIndex,
+          items: [
+            BottomNavigationBarItem(icon: currentIndex==0? Image.asset('images/f1.jpg',width:40,height:40): Image.asset('images/f1.jpg',width:20,height:20), label:'自我介紹',),
+            BottomNavigationBarItem(icon: Icon(Icons.school), label:'學習歷程',),
+            BottomNavigationBarItem(icon: Icon(Icons.schedule_outlined), label:'學習計畫',),
+            BottomNavigationBarItem(icon: Icon(Icons.engineering), label:'專業方向',),
+          ],
+          onTap: (index) {
+            setState(() {
+              previousIndex=currentIndex;
+              currentIndex=index;
+              if (index==0) {
+                if (previousIndex==currentIndex) player.resume();
+                player.stop();
+                player.play(AssetSource('1.mp3'));
+              }
+              if (index==1) {
+                if (previousIndex==currentIndex) player.resume();
+                player.stop();
+                player.play(AssetSource('2.mp3'));
+              }
+              if (index==2) {
+                if (previousIndex==currentIndex) player.resume();
+                player.stop();
+                player.play(AssetSource('3.mp3'));
+              }
+              if (index==3) {
+                if (previousIndex==currentIndex) player.resume();
+                player.stop();
+                player.play(AssetSource('4.mp3'));
+              }
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class screen1 extends StatelessWidget {
+
+  final String s1='        我出生在一個很平凡但很美滿的小家庭，父親是個工程師，母親是個會計，而姊姊已經出去工作，我還在高科大求學。父母用民主的方式管教我們，希望我們能夠獨立自主、主動學習，累積人生經驗，但他們會適時的給予鼓勵和建議，父母親只對我們要求兩件事，第一是保持健康，第二是著重課業。因為沒有健康的身體，就算有再多的才華、再大的抱負也無法發揮出來。又因為家境並不富裕，所以必須專心於課業上，學得一技之長，將來才能自立更生。'
+                  '在小學時代的我很活潑、很好動，在課業上表現平平，但運動表現不錯，曾多次參加跑步競賽。'
+                  '小學畢業後，進入了一所公立中學，在那裡我學會了許多應有的禮節與待人處世的道理。在國中時期的我，好像開了竅，開始學習程式。'
+                  '高中後，每天都覺得很充實、很快樂。高中相當快樂、大玩特玩，不過到高二下，開始不斷地努力學習，希望能夠不錯的學校就讀。在課業方面，我都能保持在一定的水準，因為上課專心聽講、仔細思考、體會老師所說的每一句話，在腦海裡架構重要觀念，一有問題就立刻發問，因此上課的吸收效率很高，不但使得複習的工作能夠很快完成，還有多餘的時間從事課外活動，我最喜歡打排球';
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          //放標題
+          Padding(
+            padding: EdgeInsets.fromLTRB(20, 30, 20, 20),
+            child: Text('Who am I', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
+          ),
+          //自傳部份
+          Container(
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.fromLTRB(30, 0, 30, 50),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black, width:3),
+              borderRadius: BorderRadius.circular(8),
+              boxShadow: [
+                BoxShadow(color: Colors.amberAccent, offset: Offset(6, 6)),
+              ],
+            ),
+            child: Text(s1, style: TextStyle(fontSize: 20)),
+          ),
+          SizedBox(height: 10,),
+          //放一張照片
+          Container(
+            color: Colors.pink,
+            child:Image.asset('images/f1.jpg'),
+            height: 200,
+            width: 200,
+          ),
+          SizedBox(height: 30),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                width: 150,
+                height:150,
+                decoration: BoxDecoration(
+                  border: Border.all(width:2, color: Colors.redAccent, style: BorderStyle.solid),
+                  borderRadius: BorderRadius.circular(30),
+                  image: DecorationImage(image: AssetImage('images/f1.jpg'), fit: BoxFit.cover),
+                ),
+                //color: Colors.white,
+              ),
+              Container(
+                width: 150,
+                height:150,
+                decoration: BoxDecoration(
+                  border: Border.all(width:2, color: Colors.redAccent, style: BorderStyle.solid),
+                  borderRadius: BorderRadius.circular(30),
+                  image: DecorationImage(image: AssetImage('images/f1.jpg'), fit: BoxFit.cover),
+                ),
+                //color: Colors.white,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class screen2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(child: Text('Screen2'),
+      ),
+    );
+  }
+}
+class screen3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text('大一時期'),
+          ],
+        ),
+        SizedBox(height: 10,),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              height:200,
+              width: 200,
+              child: ListView(
+                children: [
+                  Text('1. 學好英文'),
+                  Text('2. 程式語言'),
+                  Text('3. 考取證照'),
+                  Text('4. 人際關係'),
+                ],
+              ),
+            ),
+          ],
+        ),
+        Row(),
+        Row(),
+        Row(),
+        Row(),
+      ],),
+    );
+  }
+}
+class screen4 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(child: Text('Screen4'),
+      ),
+    );
+  }
+}
